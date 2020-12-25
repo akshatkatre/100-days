bb04d2a902e410aa42db8b6b7407172fc2088865
akshat <akshatkatre@gmail.com>
2020-12-25 17:07:28 +0530

    day 32 - smtp

--- email_32/main.py
+++ email_32/main.py
@@ -1,15 +1,37 @@
-##################### Extra Hard Starting Project ######################
 import pandas as pd
-# 1. Update the birthdays.csv
+import datetime as dt
+import random
+import smtplib
 
-# 2. Check if today matches a birthday in the birthdays.csv
+SENDER_EMAIL = ""
+PASSWORD = ""
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
-# 4. Send the letter generated in step 3 to that person's email address.
+def send_email(message_body, to_email_address):
+    with smtplib.SMTP("outlook.office365.com") as connection:
+        connection.starttls()
+        connection.login(user=SENDER_EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=SENDER_EMAIL,
+            to_addrs=to_email_address,
+            msg=f"subject:Happy Birthday!\n\n{message_body}"
+        )
 
+
+LETTER_TEMPLATES = ['letter_templates/letter_1.txt',
+                    'letter_templates/letter_2.txt',
+                    'letter_templates/letter_3.txt']
+
+now = dt.datetime.now()
+print(now.year, now.month, now.day)
 df = pd.read_csv('birthdays.csv')
-data = df.tolist()
-print(data)
-
+for index, row in df.iterrows():
+    if now.month == row.month and now.day == row.day:
+        print(row['name'])
+        with open(random.choice(LETTER_TEMPLATES)) as file_handle:
+            letter_template = "".join(file_handle.readlines())
+        birth_day_message = letter_template.replace('[NAME]', row['name'])
+        print(birth_day_message)
+        print(row['email'])
+        send_email(birth_day_message, row['email'])
 
