1e8cde73e6928974510682d7fc280938c9406570
akshat <akshatkatre@gmail.com>
2021-01-11 09:36:07 +0530

    Day 48 - Cookie clicker Selenium - Exception handling

--- selenium_48/cookie_clicker.py
+++ selenium_48/cookie_clicker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import time
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 URL = "http://orteil.dashnet.org/experiments/cookie/"
 CHROME_DRIVER_PATH = "/Users/akshat/repo/chromedriver/chromedriver"
@@ -11,57 +12,93 @@ driver.get(url=URL)
 
 
 def purchase(m_value: int):
-    buy_portal = driver.find_element_by_xpath('//*[@id="buyPortal"]/b')
-    buy_portal_value = int(buy_portal.text.replace(',', '').split(' - ')[1])
-    print(f"buy portal: {buy_portal_value}")
-    if m_value > buy_portal_value:
-        buy_portal.click()
-        return True
+    try:
+        buy_portal = driver.find_element_by_xpath('//*[@id="buyPortal"]/b')
+        buy_portal_value = int(buy_portal.text.replace(',', '').split(' - ')[1])
+        print(f"buy portal: {buy_portal_value}")
+        if m_value > buy_portal_value:
+            buy_portal.click()
+            return True
+    except NoSuchElementException:
+        print("buyPortal element not found...")
+    except StaleElementReferenceException:
+        print("buyPortal element not found...")
 
-    buy_alchemy = driver.find_element_by_xpath('//*[@id="buyAlchemy lab"]/b')
-    buy_alchemy_value = int(buy_alchemy.text.replace(',', '').split(' - ')[1])
-    print(f"buy alchemy: {buy_alchemy_value}")
-    if m_value > buy_alchemy_value:
-        buy_alchemy.click()
-        return True
+    try:
+        buy_alchemy = driver.find_element_by_xpath('//*[@id="buyAlchemy lab"]/b')
+        buy_alchemy_value = int(buy_alchemy.text.replace(',', '').split(' - ')[1])
+        print(f"buy alchemy: {buy_alchemy_value}")
+        if m_value > buy_alchemy_value:
+            buy_alchemy.click()
+            return True
+    except NoSuchElementException:
+        print('buyAlchmy element not found...')
+    except StaleElementReferenceException:
+        print("buyAlchmy element not found...")
 
-    buy_shipment = driver.find_element_by_xpath('//*[@id="buyShipment"]/b')
-    print(buy_shipment.text)
-    buy_shipment_value = int(buy_shipment.text.replace(',','').split(' - ')[1])
-    print(f"buy shipment: {buy_shipment_value}")
-    if m_value > buy_shipment_value:
-        buy_shipment.click()
-        return True
+    try:
+        buy_shipment = driver.find_element_by_xpath('//*[@id="buyShipment"]/b')
+        print(buy_shipment.text)
+        buy_shipment_value = int(buy_shipment.text.replace(',','').split(' - ')[1])
+        print(f"buy shipment: {buy_shipment_value}")
+        if m_value > buy_shipment_value:
+            buy_shipment.click()
+            return True
+    except NoSuchElementException:
+        print("buyShipment element not found...")
+    except StaleElementReferenceException:
+        print("buyShipment element not found...")
 
-    buy_mine = driver.find_element_by_xpath('//*[@id="buyMine"]/b')
-    buy_mine_value = int(buy_mine.text.replace(',','').split(' - ')[1])
-    print(f"buy mine: {buy_mine_value}")
-    if m_value > buy_mine_value:
-        buy_mine.click()
-        return True
+    try:
+        buy_mine = driver.find_element_by_xpath('//*[@id="buyMine"]/b')
+        buy_mine_value = int(buy_mine.text.replace(',','').split(' - ')[1])
+        print(f"buy mine: {buy_mine_value}")
+        if m_value > buy_mine_value:
+            buy_mine.click()
+            return True
+    except NoSuchElementException:
+        print("buyMine element not found...")
+    except StaleElementReferenceException:
+        print("buyMine element not found...")
 
-    buy_factory = driver.find_element_by_xpath('//*[@id="buyFactory"]/b')
-    buy_factory_value = int(buy_factory.text.split(' - ')[1])
-    print(f"buy factory: {buy_factory_value}")
-    if m_value > buy_factory_value:
-        buy_factory.click()
-        return True
+    try:
+        buy_factory = driver.find_element_by_xpath('//*[@id="buyFactory"]/b')
+        buy_factory_value = int(buy_factory.text.split(' - ')[1])
+        print(f"buy factory: {buy_factory_value}")
+        if m_value > buy_factory_value:
+            buy_factory.click()
+            return True
+    except NoSuchElementException:
+        print('buyFactory element not found...')
+    except StaleElementReferenceException:
+        print("buyFactory element not found...")
 
-    buy_grandma = driver.find_element_by_xpath('//*[@id="buyGrandma"]/b')
-    buy_grandma_value = int(buy_grandma.text.split(' - ')[1])
-    print(f"buy grandma: {buy_grandma_value}")
-    if m_value > buy_grandma_value:
-        buy_grandma.click()
-        return True
+    try:
+        buy_grandma = driver.find_element_by_xpath('//*[@id="buyGrandma"]/b')
+        buy_grandma_value = int(buy_grandma.text.split(' - ')[1])
+        print(f"buy grandma: {buy_grandma_value}")
+        if m_value > buy_grandma_value:
+            buy_grandma.click()
+            return True
+    except NoSuchElementException:
+        print('buyGrandma element not found...')
+    except StaleElementReferenceException:
+        print("buyGrandma element not found...")
 
-    buy_cursor = driver.find_element_by_xpath('//*[@id="buyCursor"]/b')
-    buy_cursor_value = int(buy_cursor.text.split(' - ')[1])
-    print(f"buy cursor: {buy_cursor_value}")
-    if m_value > buy_cursor_value:
-        buy_cursor.click()
-        return True
+    try:
+        buy_cursor = driver.find_element_by_xpath('//*[@id="buyCursor"]/b')
+        buy_cursor_value = int(buy_cursor.text.split(' - ')[1])
+        print(f"buy cursor: {buy_cursor_value}")
+        if m_value > buy_cursor_value:
+            buy_cursor.click()
+            return True
+    except NoSuchElementException:
+        print('buyCursor element not found...')
+    except StaleElementReferenceException:
+        print("buyCursor element not found...")
 
     return False
+
 
 cookie = driver.find_element_by_css_selector("#cookie")
 game_on = True
