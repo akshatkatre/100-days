8ce560dfda32035e61c8ebdf53fd29391cc261eb
akshatkatre <akshat.katre@gmail.com>
2021-01-19 07:48:18 +0530

    day 59, blog

--- html_bs_59/main.py
+++ html_bs_59/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from common import emailer
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
@@ -25,9 +26,27 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
-def contact():
+@app.route('/contact', methods=['get'])
+def contact_page():
     return render_template("contact.html")
+
+
+@app.route('/contact', methods=['post'])
+def process_contact():
+    print("/contact method invoked...")
+    name = request.form['name_inp']
+    email = request.form['email_inp']
+    phone = request.form['phone_inp']
+    message = request.form['message_inp']
+    email_message = f'Name: {name}\n' \
+                    f'Email: {email}\n' \
+                    f'Phone: {phone}\n' \
+                    f'Message: {message}'
+    # print(name, email, message)
+    print(email_message)
+    print("sending message...")
+    emailer.send_email(email_subject="New Contact!", message_body=email_message)
+    return render_template("contact.html", message_processed=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
